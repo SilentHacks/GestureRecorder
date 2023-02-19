f2bb0ca52bea3656b59bb8d8de4f346d875f5e6b
abid3 <zcabma1@ucl.ac.uk>
2023-02-19 17:04:22 +0000

    Reformat GestureRecorder to work as a class with same interface as PoseRecorder

--- gesture_recorder.py
+++ gesture_recorder.py
@@ -1,79 +1,14 @@
 import json
-import os
 
 import cv2
 import numpy as np
-from fastdtw import fastdtw
 from matplotlib import pyplot as plt
-from scipy.spatial.distance import euclidean
 
 from utils.config import FOCUS_POINTS, mp_drawing, mp_pose, draw_style
 from utils.tracker_2d import process_landmarks
 
-# class GestureRecorder:
-#     def __init__(self):
 
-def record(gesture_name, file_name):
-    # Check if .mov or .mp4 file exists - if it does, choose the right one
-    if not os.path.isfile(path := os.path.join("data", "videos", gesture_name, f"{file_name}.mov")):
-        path = os.path.join("data", "videos", gesture_name, f"{file_name}.mp4")
-
-    cap = cv2.VideoCapture(path)
-
-    history = {num.value: [] for num in FOCUS_POINTS}
-
-    with mp_pose.Pose(
-            static_image_mode=True,
-            model_complexity=1,
-            min_detection_confidence=0.5
-    ) as pose:
-        while cap.isOpened():
-            ret, image = cap.read()
-            if not ret:
-                break
-
-            image.flags.writeable = False
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            results = pose.process(image)
-
-            # Draw the pose annotation on the image.
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            mp_drawing.draw_landmarks(image, results.pose_landmarks,  # type: ignore
-                                      mp_pose.POSE_CONNECTIONS, landmark_drawing_spec=draw_style)
-
-            if results.pose_world_landmarks:  # type: ignore
-                for num in FOCUS_POINTS:
-                    landmark = results.pose_world_landmarks.landmark[num.value]  # type: ignore
-                    history[num.value].append((landmark.x, landmark.y) if landmark else (0, 0))
-
-            cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
-            if cv2.waitKey(5) & 0xFF == 27:
-                break
-
-        cap.release()
-
-    print('Frames:', len(history[list(history.keys())[0]]))
-
-    return history
-
-
-gesture = 'relax'
-
-
-def main():
-    history = record(gesture, '1')
-    processed = process_landmarks(history, plot=True)
-
-    save_json(processed)
-
-
-def save_json(processed):
-    with open(f'data/models/gestures/{gesture}.json', 'w') as f:
-        json.dump(processed, f, indent=4)
-
-
-def plot_json():
+def plot_json(gesture: str = 'punch'):
     with open(f'data/models/gestures/{gesture}.json', 'r') as f:
         data = json.load(f)
 
@@ -88,22 +23,68 @@ def plot_json():
     plt.show()
 
 
-def compare():
-    with open(f'data/models/gestures/{gesture}.json', 'r') as f:
-        model = json.load(f)
+class GestureRecorder:
+    def __init__(
+            self,
+            camera=None,
+            min_detection_confidence: float = 0.5,
+            min_tracking_confidence: float = 0.5,
+            model_complexity: int = 1,
+            save_dir: str = 'data/models/gestures',
+    ):
+        """
+        Initialize the recorder.
 
-    history = record(gesture, '2')
-    landmark_ids = {int(idx) for idx in model.keys()}
-    processed = process_landmarks(history, include_landmarks=landmark_ids, plot=True)
+        :param camera: camera ID to use
+        :param min_detection_confidence: the minimum confidence for detection
+        :param min_tracking_confidence: the minimum confidence for tracking
+        :param model_complexity: the complexity of the model
+        :param save_dir: the directory to save the poses
+        """
+        self.capture = cv2.VideoCapture(camera)
+        self.min_detection_confidence = min_detection_confidence
+        self.min_tracking_confidence = min_tracking_confidence
+        self.model_complexity = model_complexity
+        self.save_dir = save_dir
 
-    distances = []
-    for landmark_id, points in model.items():
-        distance, _ = fastdtw(processed[int(landmark_id)], points, dist=euclidean)
-        # distance = calculate_threshold(points, processed[int(landmark_id)])
-        distances.append(distance)
+    def save_json(self, processed, name):
+        with open(f'{self.save_dir}/{name}.json', 'w') as f:
+            json.dump(processed, f, indent=4)
 
-    print(distances, sum(distances) / len(distances))
+    def record(self, name: str):
+        history = {num.value: [] for num in FOCUS_POINTS}
 
+        with mp_pose.Pose(
+                static_image_mode=False,
+                model_complexity=self.model_complexity,
+                min_detection_confidence=self.min_detection_confidence,
+                min_tracking_confidence=self.min_tracking_confidence
+        ) as pose:
+            while self.capture.isOpened():
+                ret, image = self.capture.read()
+                if not ret:
+                    break
 
-if __name__ == '__main__':
-    main()
+                image.flags.writeable = False
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                results = pose.process(image)
+
+                # Draw the pose annotation on the image.
+                image.flags.writeable = True
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                mp_drawing.draw_landmarks(image, results.pose_landmarks,  # type: ignore
+                                          mp_pose.POSE_CONNECTIONS, landmark_drawing_spec=draw_style)
+
+                if results.pose_world_landmarks:  # type: ignore
+                    for num in FOCUS_POINTS:
+                        landmark = results.pose_world_landmarks.landmark[num.value]  # type: ignore
+                        history[num.value].append((landmark.x, landmark.y) if landmark else (0, 0))
+
+                cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+                if cv2.waitKey(5) & 0xFF == 27:
+                    break
+
+            self.capture.release()
+
+        processed = process_landmarks(history, plot=True)
+        self.save_json(processed, name)
