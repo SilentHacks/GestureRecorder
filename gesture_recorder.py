Use correct normalization technique for accurate gesture detection in 3D

gesture_recorder.py
@@ -9,6 +9,7 @@ from scipy.spatial.distance import euclidean
 
 from utils.config import FOCUS_POINTS, mp_drawing, mp_pose, draw_style
 from utils.tracker_2d import process_landmarks
+from utils.tracker_3d import process_landmarks_3d
 
 
 def record(gesture_name, file_name):
@@ -43,7 +44,7 @@ def record(gesture_name, file_name):
             if results.pose_world_landmarks:  # type: ignore
                 for num in FOCUS_POINTS:
                     landmark = results.pose_world_landmarks.landmark[num.value]  # type: ignore
-                    history[num.value].append((landmark.x, landmark.y) if landmark else (0, 0))
+                    history[num.value].append((landmark.x, landmark.y, landmark.z) if landmark else (0, 0, 0))
 
             cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
             if cv2.waitKey(5) & 0xFF == 27:
@@ -56,18 +57,18 @@ def record(gesture_name, file_name):
     return history
 
 
-gesture = 'tennis_swing'
+gesture = 'baseball_swing'
 
 
 def main():
-    history = record(gesture, '14')
-    processed = process_landmarks(history, plot=True)
+    history = record(gesture, '12')
+    processed = process_landmarks_3d(history, plot=True)
 
     save_json(processed)
 
 
 def save_json(processed):
-    with open(f'data/models/gestures/{gesture}2.json', 'w') as f:
+    with open(f'data/models/gestures/{gesture}3d.json', 'w') as f:
         json.dump(processed, f, indent=4)
 
 

utils/tracker_3d.py
@@ -8,18 +8,22 @@ from matplotlib import pyplot as plt
 from itertools import islice
 
 
-def normalize_gesture_3d(gesture):
+def normalize_gesture_3d(gesture, axis_length=None):
     x, y, z = zip(*gesture)
-    x_min, x_max, y_min, y_max, z_min, z_max = min(x), max(x), min(y), max(y), min(z), max(z)
-    x_axis_length = x_max - x_min
-    y_axis_length = y_max - y_min
-    axis_length = max(x_axis_length, y_axis_length) or 1
     x_average = sum(x) / len(x)
     y_average = sum(y) / len(y)
     z_average = sum(z) / len(z)
+
+    if axis_length is None:
+        x_min, x_max, y_min, y_max, z_min, z_max = min(x), max(x), min(y), max(y), min(z), max(z)
+        x_axis_length = x_max - x_min
+        y_axis_length = y_max - y_min
+        axis_length = max(x_axis_length, y_axis_length) or 1
+
     normalized_x = [(x_i - x_average) / axis_length for x_i in x]
     normalized_y = [(y_i - y_average) / axis_length for y_i in y]
     normalized_z = [(z_i - z_average) / axis_length for z_i in z]
+
     return normalized_x, normalized_y, normalized_z
 
 
@@ -92,12 +96,20 @@ def gaussian_filter_3d(points: list[tuple[int, int, int]], sigma: float = 1.0):
 def select_landmarks_3d(landmark_history: dict[int, list[tuple[int, int, int]]]):
     """Select the relevant landmarks from the tracking points based on the variance of its signal"""
     good_landmarks = set()
-    thresh = 0.125
-    for landmark_id, tracking_points in landmark_history.items():
-        # normalized = normalize_gesture(tracking_points)
-        # smoothed_points = laplacian_smoothing_3d(list(zip(*normalized)))
+    max_axis_length = 0
 
-        smoothed_points = laplacian_smoothing_3d(tracking_points)
+    for landmark_id, tracking_points in landmark_history.items():
+        tracking_points = np.array(tracking_points)
+        axis_length = max(tracking_points[:, 0].ptp(), tracking_points[:, 1].ptp()) or 1
+        if axis_length > max_axis_length:
+            max_axis_length = axis_length
+
+    thresh = 0.08
+    for landmark_id, tracking_points in landmark_history.items():
+        normalized = normalize_gesture_3d(tracking_points, axis_length=max_axis_length)
+        smoothed_points = laplacian_smoothing_3d(list(zip(*normalized)))
+
+        # smoothed_points = laplacian_smoothing_3d(tracking_points)
 
         # Check the variance of the signal to determine if the landmark is relevant
         x_values, y_values, z_values = zip(*smoothed_points)
@@ -105,18 +117,23 @@ def select_landmarks_3d(landmark_history: dict[int, list[tuple[int, int, int]]])
         y_var = np.var(y_values)
         z_var = np.var(z_values)
         # print(landmark_id, x_var, y_var, z_var)
-        if x_var > thresh or y_var > thresh or z_var > thresh:
+        if x_var > thresh or y_var > thresh or z_var > 0.02:
             good_landmarks.add(landmark_id)
 
     # print(len(good_landmarks))
     return good_landmarks
 
 
-def process_landmarks_3d(landmark_history: dict[..., list[tuple[int, int, int]]],
-                         relevant_landmarks: set[int] = None, plot: bool = False):
+def process_landmarks_3d(landmark_history: dict[..., list[tuple[int, int, int]]], include_landmarks: set[int] = None,
+                         exclude_landmarks: set[int] = None, plot: bool = False):
     """Process the landmark history to select relevant landmarks and simplify the tracking points"""
     # Select the relevant landmarks
-    good_landmarks = select_landmarks_3d(landmark_history).union(relevant_landmarks or set())
+    good_landmarks = select_landmarks_3d(landmark_history)
+
+    if include_landmarks:
+        good_landmarks |= include_landmarks
+    if exclude_landmarks:
+        good_landmarks -= exclude_landmarks
 
     # Simplify the tracking points for each landmark
     simplified_landmarks = {}
@@ -129,7 +146,7 @@ def process_landmarks_3d(landmark_history: dict[..., list[tuple[int, int, int]]]
         # simplified = simplify_gesture_3d(landmark_history[landmark_id], tolerance=0.01)
         simplified = landmark_history[landmark_id]
 
-        smoothed_points = np.array(gaussian_filter_3d(simplified, sigma=1))
+        smoothed_points = np.array(gaussian_filter_3d(simplified, sigma=2))
         smoothed_points -= np.mean(smoothed_points, axis=0)
 
         # smoothed_points = np.array(smooth_gesture(tracking_points))