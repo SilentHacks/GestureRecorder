d333b3268d36b788df99a51c256302890eb842e7
Weiyi Zhang <zcabwz1@ucl.ac.uk>
2023-02-23 05:57:18 +0000

    Ready to merge with the modified MFC

--- utils/ThumbNailUtils.py
+++ utils/ThumbNailUtils.py
@@ -1,16 +1,16 @@
-# from moviepy.editor import VideoFileClip, vfx
+from moviepy.editor import VideoFileClip, vfx
 import os
 import cv2
 
 class ThumbNailUtils:
 
-    # def gifVideoCvt(videoFilePath: str, gestureName: str = None):
-    #     clip = VideoFileClip(videoFilePath)
-    #     dataPath = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "gui/assets/thumbnails/gestureGifs/")
-    #     # fileName = videoFilePath.split("/")[-1][:-4]
-    #     fileName = gestureName
-    #     reversed_clip = clip.fx(vfx.mirror_x)
-    #     reversed_clip.write_gif(fps=8, filename=dataPath + fileName + ".gif")
+    def gifVideoCvt(videoFilePath: str, gestureName: str = None):
+        clip = VideoFileClip(videoFilePath)
+        dataPath = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "gui/assets/thumbnails/gestureGifs/")
+        # fileName = videoFilePath.split("/")[-1][:-4]
+        fileName = gestureName
+        reversed_clip = clip.fx(vfx.mirror_x)
+        reversed_clip.write_gif(fps=8, filename=dataPath + fileName + ".gif")
 
     def frameImgCvt(name=None, frame=None):
         dataPath = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "gui/assets/thumbnails/posePics/")
