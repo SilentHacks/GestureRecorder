186feacab47385b9c355caa2d6c33e9c66274cd7
Weiyi Zhang <93227235+DEmonPE@users.noreply.github.com>
2023-02-27 14:16:17 +0000

    Ready to revert changes

--- utils/VideoRecorder.py
+++ utils/VideoRecorder.py
@@ -1,5 +1,9 @@
+import time
+
 import cv2
 import os
+import threading
+import queue
 """ simple video recorder, opens a cv window via webcam
     user press 'r' to start recording, the recording length will be <= 2 seconds """
 
@@ -13,7 +17,6 @@ class VideoRecorder:
         self.name = name
         self.fps = 17
         self.size = (960, 720)
-        self.video = None
         self.frame_count = 0
         self.recording = False
         self.dataPath = os.path.join(os.path.abspath(os.path.join(os.path.join(os.path.dirname(__file__), os.pardir), "data")), "videos")
@@ -23,18 +26,53 @@ class VideoRecorder:
         self.cap = cv2.VideoCapture(camera)
         # cv2.resizeWindow('Gesture Recorder', self.size[0], self.size[1])
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.size[0])
+        self.video = None
+        self.frame_queue = queue.Queue()
+
+    def record_video(self, frame_queue):
+        global recording  # Use global variable for recording status
+        while True:
+            # Get a frame from the queue if available
+            if not frame_queue.empty():
+                frame = frame_queue.get()
+                vidout = cv2.resize(frame, self.size)
+                # Write the frame into the file 'output.mp4'
+                if recording:
+                    print("writing")
+                    self.video.write(vidout)
+
     def run(self):
-        print(self.save_file)
         self.start()
+        print(self.save_file)
+        record_thread = threading.Thread(target=self.record_video(frame_queue=self.frame_queue))
+        record_thread.start()
+
         while True:
             # print("recording: ", self.recording)
             _, frame = self.cap.read()
             cv2.imshow('Gesture Recorder', cv2.flip(frame, 1))
             key = cv2.waitKey(1)
-            if self.recording:
-                self.record(frame=frame)
-            if self.handle_key(key=key):
+
+            if key == ord('r'):
+                print("Recording will start in 3 seconds...")
+                time.sleep(1)
+                print("Recording will start in 2 seconds...")
+                time.sleep(1)
+                print("Recording will start in 1 seconds...")
+                time.sleep(1)
+
+                print("Recording...")
+                self.recording = True
+
+                # If q is pressed, stop recording and quit
+            elif key == ord('q'):
+                print("Recording stopped.")
+                self.recording = False
                 break
+
+            elif key == 27:
+                break
+            self.frame_queue.put(frame)
 
         self.cap.release()
         self.video.release()
@@ -43,28 +81,26 @@ class VideoRecorder:
             # press 'r' to start recording press again to stop
 
 
-    def handle_key(self, key: int) -> bool:
-        if key == ord('r'):
-            if self.recording:
-                print('stop recording')
-                self.stop()
-                self.recording = False
-                return False
-            else:
-                self.recording = True
-                return False
-        elif key == 27:
-            return True
-
+    # def handle_key(self, key: int) -> bool:
+    #     if key == ord('r'):
+    #         if self.recording:
+    #             print('stop recording')
+    #             self.stop()
+    #             self.recording = False
+    #             return False
+    #         else:
+    #             time.sleep(1)
+    #             print("recording starts in 3")
+    #             time.sleep(1)
+    #             print("recording starts in 2")
+    #             time.sleep(1)
+    #             print("recording starts in 1")
+    #             time.sleep(1)
+    #             self.recording = True
+    #             return False
+    #     elif key == 27:
+    #         return True
     def start(self):
-        """
-        Start the video recorder.
-        :return:
-        """
-        # print("here")
-        # print("video save file: ", self.dataPath)
-        # if not os.path.exists(self.save_file):
-        #     os.mkdir(self.dataPath)
         self.video = cv2.VideoWriter(self.save_file, cv2.VideoWriter_fourcc(*'MJPG'), self.fps, self.size)
 
     def record(self, frame):
---
+++ utils/rec.py
@@ -0,0 +1,84 @@
+import cv2
+import time
+import threading
+import queue
+
+# Create a VideoCapture object
+cap = cv2.VideoCapture(1)
+
+# Check if camera opened successfully
+if (cap.isOpened() == False):
+    print("Unable to read camera feed")
+
+# Default resolutions of the frame are obtained.The default resolutions are system dependent.
+# We convert the resolutions from float to integer.
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+
+# Define the codec for output video
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+
+# Create a VideoWriter object.The output is stored in 'output.mp4' file.
+out = cv2.VideoWriter('output.mp4', fourcc, 17.0, (960, 540))
+
+# Initialize a flag for recording status
+recording = False
+
+# Initialize a queue for storing frames
+frame_queue = queue.Queue()
+
+
+# Define a function for recording video in a separate thread
+def record_video():
+    global recording  # Use global variable for recording status
+    while True:
+        # Get a frame from the queue if available
+        if not frame_queue.empty():
+            frame = frame_queue.get()
+            # Write the frame into the file 'output.mp4'
+            if recording:
+                out.write(frame)
+
+
+# Create a thread for recording video and start it
+record_thread = threading.Thread(target=record_video)
+record_thread.start()
+
+while (True):
+    ret, frame = cap.read()
+
+    if ret == True:
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+
+        # Wait for user input
+        key = cv2.waitKey(1) & 0xFF
+
+        # If r is pressed, start recording after 3 seconds countdown
+        if key == ord('r'):
+            print("Recording will start in 3 seconds...")
+            time.sleep(3)
+            print("Recording...")
+            recording = True
+
+        # If q is pressed, stop recording and quit
+        elif key == ord('q'):
+            print("Recording stopped.")
+            recording = False
+            break
+
+
+        # Put the frame into the queue for recording thread to consume
+        frame_queue.put(frame)
+
+    # Break the loop
+    else:
+        break
+
+    # When everything done, release the video capture and video write objects
+cap.release()
+out.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
